612fdb9ee3371bae28377983798a77c03eb75614
Paper02 <Holacasa898@gmail.com>
2025-07-23 20:06:23 -0400

    Adds the new page

--- frontend/real-estate-price-predictor-app/src/App.js
+++ frontend/real-estate-price-predictor-app/src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import Welcome from "./components/welcome";
 import Predictor from "./pages/predictor/Predictor";
-
+import Explorer from "./pages/explorer/Explorer";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/predictor" element={<Predictor />} />
+        <Route path="/explorer" element={<Explorer />} />
         {/* Add more routes for other pages as needed */}
       </Routes>
     </div>
---
+++ frontend/real-estate-price-predictor-app/src/pages/explorer/Explorer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Explorer() {
+    return (
+        <div>
+            <h1>Real Estate Price Explorer</h1>
+            {/* Add your explorer components and logic here */}
+        </div>
+    );
+};
